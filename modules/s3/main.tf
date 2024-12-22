a732eccd19ab63c85292b11477c53adf9cfe4e46
Travis Pollard <tpollard@Traviss-MacBook-Air.local>
2024-12-21 20:03:37 -0600

    many changes to S3 module, reverted to public policy

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,32 @@
-resource "aws_s3_bucket_website_configuration" "travispollard.com" {
-  bucket = aws_s3_bucket.example.id
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = "Terraform"
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.this.id}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_website_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
 
   index_document {
     suffix = "index.html"
@@ -7,14 +34,5 @@ resource "aws_s3_bucket_website_configuration" "travispollard.com" {
 
   error_document {
     key = "error.html"
-  }
-
-  routing_rule {
-    condition {
-      key_prefix_equals = "docs/"
-    }
-    redirect {
-      replace_key_prefix_with = "documents/"
-    }
   }
 }
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -2,3 +2,13 @@ variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
 }
+
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "cloudfront_distribution_id" {
+  description = "The ID of the CloudFront distribution associated with this bucket"
+  type        = string
+}
