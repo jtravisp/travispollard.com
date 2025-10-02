e327c8a47a354855e5bac048260efb8497bb53fe
Travis Pollard <tpollard@Traviss-MacBook-Air.local>
2025-10-01 19:02:17 -0500

    fix embed link

--- frontend/app/campout/page.tsx
+++ frontend/app/campout/page.tsx
@@ -2,7 +2,7 @@ export default function Campout() {
   return (
     <main className="min-h-screen bg-base-100 text-base-content">
       <iframe
-        src="https://cream-pheasant-327.notion.site/TL-411-Lake-Brownwood-Campout-Fall-2025-27dfb209725980d1bb65c651e59fc41c"
+        src="https://cream-pheasant-327.notion.site/ebd/27dfb209725980d1bb65c651e59fc41c"
         className="w-full h-screen md:h-[90vh] border-none"
         allowFullScreen
       />
